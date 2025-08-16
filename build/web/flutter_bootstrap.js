814a40931e3be5a12a421fd6825f22a3e85c8380
Prabhudev <prabh@example.com>
2025-08-16 19:16:35 +0530

    Deploy Phase 1 OMS: Admin panel with Order Management tab and comprehensive OMS controls

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1110810097"
+    serviceWorkerVersion: "630369960"
   }
 });
